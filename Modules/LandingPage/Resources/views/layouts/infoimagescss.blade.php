beda8bf3cd5e67d40516111a9e960a1f89c90cdb
elphabayinc <elphabayinc@gmail.com>
2024-08-12 23:13:40 +0100

    'first'

---
+++ Modules/LandingPage/Resources/views/layouts/infoimagescss.blade.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="{{ asset('assets/assets/previewlinksBlog.css')}}">
+<link rel="stylesheet" href="{{ asset('assets/assets/swiper/dist/css/swiper.min.css')}}">
+<link rel="stylesheet" href="{{ asset('assets/custom/custom.css')}}">
---
+++ Modules/LandingPage/Resources/views/layouts/infoimagesjs.blade.php
@@ -0,0 +1,4 @@
+<script src="{{ asset('assets/assets/swiper/dist/js/swiper.min.js')}}"></script>
+<script src="{{ asset('assets/assets/previewlinksBlog.js') }}"></script>
+<script src="{{ asset('assets/custom/custom.js') }}"></script>
+
